045d99a33597f0fa133e12bfe1dc409581be9d1f
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-21 19:27:36 +0000

    LabCompleted

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -27,6 +27,14 @@ public class GameService {
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
-        return false;
+        if( guessNumber == secretNumber)
+        {
+            return true;
+            //System.out.println();
+        }
+        else
+        {
+            return false;
+        }
     }
 }
